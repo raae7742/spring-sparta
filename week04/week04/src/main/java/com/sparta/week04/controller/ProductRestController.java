2d6416702a682797b761c5039f18c686a301095b
raae7742 <aeae_1@naver.com>
2021-07-27 03:15:35 +0900

    4주차

---
+++ week04/week04/src/main/java/com/sparta/week04/controller/ProductRestController.java
@@ -0,0 +1,30 @@
+package com.sparta.week04.controller;
+
+import com.sparta.week04.models.Product;
+import com.sparta.week04.models.ProductRepository;
+import com.sparta.week04.models.ProductRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ProductRestController {
+    private final ProductRepository productRepository;
+
+    @GetMapping("/api/products")
+    public List<Product> readProducts() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        return productRepository.save(product);
+    }
+}
